541d6890ca9715c65b8d6996abfb00cfec989bd8
hf <274414334@qq.com>
2020-10-26 19:01:10 +0800

    Merge branch 'dev_DataEdit' of D:\work\datax-web with conflicts.

--- datax-search/src/main/java/com/wugui/datax/search/filter/JWTAuthenticationFilter.java
+++
@@ -1,7 +0,0 @@
-package com.wugui.datax.search.filter;
-
-/**
- * Created by iwlnner on 2020/9/11.
- */
-public class JWTAuthenticationFilter {
-}
--- datax-search/src/main/java/com/wugui/datax/search/service/impl/SearchServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.wugui.datax.search.service.impl;
-
-import com.wugui.datax.search.service.ISearchService;
-
-/**
- * Created by iwlnner on 2020/9/12.
- */
-public class SearchServiceImpl implements ISearchService{
-}
