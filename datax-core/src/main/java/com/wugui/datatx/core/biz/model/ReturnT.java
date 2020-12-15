e7ba5d1f92245075aa4a0f0006f79702720c27f8
775459833@qq.com <caohaisheng1996>
2020-12-15 10:24:08 +0800

    修改BUG

--- datax-core/src/main/java/com/wugui/datatx/core/biz/model/ReturnT.java
+++ datax-core/src/main/java/com/wugui/datatx/core/biz/model/ReturnT.java
@@ -29,10 +29,7 @@ public class ReturnT<T> implements Serializable {
 		this.code = SUCCESS_CODE;
 		this.content = content;
 	}
-	public ReturnT(String msg){
-		this.code = 200;
-		this.msg = msg;
-	}
+
 
 	public int getCode() {
 		return code;
